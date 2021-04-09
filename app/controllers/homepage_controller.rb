841dc6954735b3fbe8ac23ecc87cc0d986a30e7b
Joshua Waiswa <jwaiswa7@gmail.com>
2021-04-09 14:37:45 +0300

    Adding bootstrap

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,5 @@
 class HomepageController < ApplicationController
   def index
+  	@tickers = Ticker.all
   end
 end
--- app/models/ticker.rb
+++ app/models/ticker.rb
@@ -2,6 +2,6 @@ class Ticker < ApplicationRecord
 	SYMBOLS = %w[BTCUSDT ETHUSDT LTCUSDT]
 	validates :symbol, presence: true
     validates_inclusion_of :symbol, :in => SYMBOLS
-    validates :symbol, uniqueness: true
+    validates :symbol, uniqueness: true, on: :create
 
 end
